e36e81bf85015de6fa02693e6f74cbb42d92ba81
averybrown <averybrown718@gmail.com>
2022-11-09 13:07:32 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("https");
+
+http
+  .createServer(function (request, response){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Sample App\n");
+  })
+  .listen(8081)
+
+console.log("Server running at http://127.0.0.1:8081")
